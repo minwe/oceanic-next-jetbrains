6932aed121ec5ac54ff8e24ed48fb2bb893caf4e
minwe <minwe@yunshipei.com>
2015-11-03 10:53:05 +0800

    add colors scheme config file

--- buildjar.sh
+++ buildjar.sh
@@ -12,11 +12,17 @@ cp Oceanic\ Next.icls colors
 # needed to be able to import the JAR using "Import Settings..."
 touch IntelliJ\ IDEA\ Global\ Settings
 
+# Create config file
+echo -e '<application>\n  <component name="EditorColorsManagerImpl">\n    <global_color_scheme name="Oceanic Next" />\n  </component>\n</application>' > colors.scheme.xml
+mkdir options
+mv colors.scheme.xml options
+
 # Create the JAR file
-jar cfM settings.jar IntelliJ\ IDEA\ Global\ Settings colors
+jar cfM settings.jar IntelliJ\ IDEA\ Global\ Settings colors options
 
 # Cleanup
 rm -r colors
+rm -r options
 rm IntelliJ\ IDEA\ Global\ Settings
 
 echo "settings.jar generated"
